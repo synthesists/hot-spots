b14ca17ef9c43b8b86caa194243caef9af044d9d
CForrest97 <craforrest97@gmail.com>
2020-05-22 11:21:04 +0100

    add-theming
    Signed-off-by: CForrest97 <craforrest97@gmail.com>

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,12 @@
-import Head from 'next/head';
 import React from 'react';
 
-import styles from './index.module.scss';
+import Box from '../components/Box';
+import MainLayout from '../layouts/main';
 
-export default () => (
-  <div className={styles.Container}>
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="favicon.ico" />
-    </Head>
-    <div className="container">Hello, from the client</div>
-  </div>
-);
+export default () => {
+  return (
+    <MainLayout title="Hot Spots">
+      <Box />
+    </MainLayout>
+  );
+};
