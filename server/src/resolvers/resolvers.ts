403195cb23ee5ef699f9336476e4ceb3c8ffb1a8
matt emerson <matthewwemerson@gmail.com>
2020-06-12 18:13:20 +0100

    refactored resolvers into services

--- server/src/resolvers/resolvers.ts
+++ server/src/resolvers/resolvers.ts
@@ -1,57 +1,58 @@
 import axios from 'axios';
-import getTokenId from '../spotifyServices';
+// import getTokenId from '../spotifyServices';
 import rp from 'request-promise';
 import ITrackID from '../doc/ITrackID';
 import IPlaylistID from '../doc/IPlayistID';
-
+import {getTrack, getRecommendedTrack, getPlaylistTracks} from '../services/spotifyServices'
 
 const resolver = {
 
     playlist: async ({playlistID}: IPlaylistID) => {
-        const tokenId = await getTokenId();
-        const options = {
-            url: `https://api.spotify.com/v1/playlists/${playlistID}`,
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-                Authorization: `Bearer ${tokenId},`
-            },
-            json: true,
-        };
-        const response = await axios(options);
+        // const tokenId = await getTokenId();
+        // const options = {
+        //     url: `https://api.spotify.com/v1/playlists/${playlistID}`,
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         Accept: 'application/json',
+        //         Authorization: `Bearer ${tokenId},`
+        //     },
+        //     json: true,
+        // };
+        // const response = await axios(options);
+        const response = getPlaylistTracks(playlistID);
         return response.data
     },
 
     tracks: async ({ id }: ITrackID) => {
-        const recommendedTrack = await getRecommendedTrack(id);        
-        return recommendedTrack
+        const selectedTrack = await getTrack(id);
+        const recommendedTrack = await getRecommendedTrack(id);
+        return [selectedTrack, recommendedTrack];
+        // const recommendedTrack = await getRecommendedTrack(id);        
+        // return recommendedTrack
 
     },
     
 }
 
-const getRecommendedTrack = async (id: String) => {
-    const { tracks }:any = await getRecommendedTracks(id);
-    const [ track ] = tracks.slice(0,1)
-    return track;
-};
+// const getRecommendedTrack = async (id: String) => {
+//     const { tracks }:any = await getRecommendedTracks(id);
+//     const [ track ] = tracks.slice(0,1)
+//     return track;
+// };
 
-const getRecommendedTracks = async (id: String) => {    
-    const tokenId = await getTokenId();
-    const options = {
-    url:`https://api.spotify.com/v1/recommendations?market=US&seed_tracks=${id}&min_energy=0.4&min_popularity=10`,
-    headers: {
-      'Content-Type': 'application/json',
-    Accept: 'application/json',
-    Authorization: `Bearer ${tokenId}`,
-    },
-    json: true,
-  };
-  return rp(options);
-
-  
-
-}
+// const getRecommendedTracks = async (id: String) => {    
+//     const tokenId = await getTokenId();
+//     const options = {
+//     url:`https://api.spotify.com/v1/recommendations?market=US&seed_tracks=${id}&min_energy=0.4&min_popularity=10`,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     Accept: 'application/json',
+//     Authorization: `Bearer ${tokenId}`,
+//     },
+//     json: true,
+//   };
+//   return rp(options);
+// }
 
 
 
---
+++ server/src/services/spotifyServices.ts
@@ -0,0 +1,94 @@
+import rp from 'request-promise';
+import spotifyCredentials from '../config/config'
+import axios from 'axios';
+import ITrackID from '../doc/ITrackID';
+import IPlaylistID from '../doc/IPlayistID';
+
+let token: any;
+
+const validToken = () => token && token.expiration > new Date().getTime();
+
+const updateToken = async () => {
+    const buffer = Buffer.from(`${spotifyCredentials.clientId}:${spotifyCredentials.clientSecret}`, 'utf-8');
+
+    const options = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            grant_type: 'client_credentials',
+        },
+        json: true,
+        headers: {
+            Authorization: `Basic ${buffer.toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {},
+    };
+    const response: any = await rp(options);
+    token = {
+        id: response.access_token,
+        expiration: new Date().getTime() + response.expires_in * 1000
+    };
+}
+
+const getTokenId = async () => {
+  if (!validToken()) await updateToken();
+  return token.id;
+};
+
+const getPlaylistTracks = async ({ playlistID }: IPlaylistID) => {
+    const tokenId = await getTokenId();
+    const options = {
+        url: `https://api.spotify.com/v1/playlists/${playlistID}`,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${tokenId},`
+        },
+        json: true,
+    };
+    const response = await axios(options);
+    return response
+}
+
+const getTrack = async ({ id }: ITrackID) => {
+    const tokenId = await getTokenId();
+    const options = {
+        url: `https://api.spotify.com/v1/track/${id}`,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${tokenId},`
+        },
+        json: true,
+    };
+    const response = await axios(options);
+    return response;
+};
+
+const getRecommendedTrack = async (id: String) => {
+    const { tracks }:any = await getRecommendedTracks(id);
+    const [ track ] = tracks.slice(0,1)
+    return track;
+};
+
+const getRecommendedTracks = async (id: String) => {    
+    const tokenId = await getTokenId();
+    const options = {
+    url:`https://api.spotify.com/v1/recommendations?market=US&seed_tracks=${id}&min_energy=0.4&min_popularity=10`,
+    headers: {
+      'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${tokenId}`,
+    },
+    json: true,
+  };
+  return rp(options);
+}
+
+export default {
+    getTokenId,
+    getTrack,
+    getRecommendedTrack,
+    getPlaylistTracks
+}
