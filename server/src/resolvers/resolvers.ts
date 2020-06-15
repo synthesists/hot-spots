3bb29c4337f4b842f0e30ce360a2d79fab6f76b2
matt emerson <matthewwemerson@gmail.com>
2020-06-15 12:42:20 +0100

    only return songs with preview url

--- server/src/resolvers/resolvers.ts
+++ server/src/resolvers/resolvers.ts
@@ -1,59 +1,20 @@
-import axios from 'axios';
-// import getTokenId from '../spotifyServices';
-import rp from 'request-promise';
 import ITrackID from '../doc/ITrackID';
 import IPlaylistID from '../doc/IPlayistID';
 import {getTrack, getRecommendedTrack, getPlaylistTracks} from '../services/spotifyServices'
 
 const resolver = {
 
-    playlist: async ({playlistID}: IPlaylistID) => {
-        // const tokenId = await getTokenId();
-        // const options = {
-        //     url: `https://api.spotify.com/v1/playlists/${playlistID}`,
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         Accept: 'application/json',
-        //         Authorization: `Bearer ${tokenId},`
-        //     },
-        //     json: true,
-        // };
-        // const response = await axios(options);
+    playlist: async (playlistID: IPlaylistID) => {
         const response = getPlaylistTracks(playlistID);
-        return response.data
+        return response
     },
 
-    tracks: async ({ id }: ITrackID) => {
+    tracks: async (id: ITrackID) => {
         const selectedTrack = await getTrack(id);
-        const recommendedTrack = await getRecommendedTrack(id);
+        const recommendedTrack = await getRecommendedTrack(id);        
         return [selectedTrack, recommendedTrack];
-        // const recommendedTrack = await getRecommendedTrack(id);        
-        // return recommendedTrack
-
     },
     
 }
-
-// const getRecommendedTrack = async (id: String) => {
-//     const { tracks }:any = await getRecommendedTracks(id);
-//     const [ track ] = tracks.slice(0,1)
-//     return track;
-// };
-
-// const getRecommendedTracks = async (id: String) => {    
-//     const tokenId = await getTokenId();
-//     const options = {
-//     url:`https://api.spotify.com/v1/recommendations?market=US&seed_tracks=${id}&min_energy=0.4&min_popularity=10`,
-//     headers: {
-//       'Content-Type': 'application/json',
-//     Accept: 'application/json',
-//     Authorization: `Bearer ${tokenId}`,
-//     },
-//     json: true,
-//   };
-//   return rp(options);
-// }
-
-
 
 export default resolver;
